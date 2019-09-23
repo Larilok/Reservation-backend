9e692cb9bcb1999d7653986ea55eb9b6fdede3af
Larilok <michaelmarkelovv@gmail.com>
2019-09-24 01:58:01 +0300

    Succes sql

--- main.js
+++ main.js
@@ -4,4 +4,20 @@ let db = require('./db.js');
 
 // let db = new dba.dbAccess().getInstance(dba.pool);
 
+<<<<<<< HEAD
 // console.log(db);
+=======
+const fields = ['schemaname', 'tablename', 'tableowner'].join(', ');
+const sql = `SELECT ${fields} FROM pg_tables WHERE tableowner = $1 and schemaname = $2`;
+db.query(sql, ['postgres', 'public'], (err, res) => {
+  if (err) {
+    throw err;
+  }
+  console.dir({ res });
+  console.table(res.fields);
+  console.table(res.rows);
+  db.end();
+});
+// console.log(db);
+
+>>>>>>> Succes sql
