1bd94784a5f8ec158cf421244dbf149c5bb2a81d
IlyaLaska <IlyaLaska@users.noreply.github.com>
2019-09-24 02:47:31 +0300

    changes

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ let db = require('./db.js');
 // const fields = ['schemaname', 'tablename', 'tableowner'].join(', ');
 // const sql = `SELECT ${fields} FROM pg_tables WHERE tableowner = $1 and schemaname = $2`;
 let base = new db();
-// cons
-// base.query(sql, ['postgres', 'public'], (err, res) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.dir({ res });
-//   console.table(res.fields);
-//   console.table(res.rows);
-//   db.end();
-// });
+// console.log(base.query('SELECT * from inventory'));
+let sql = 'SELECT * from inventory';
+base.query(sql, (err, res) => {
+  if (err) {
+    throw err;
+  }
+  console.dir({ res });
+  console.table(res.fields);
+  console.table(res.rows);
+  base.end();
+});
 // console.log(db);
 
