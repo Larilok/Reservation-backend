489c1417ade379253ae3185bd07004d05398bafc
IlyaLaska <IlyaLaska@users.noreply.github.com>
2019-10-16 22:36:23 +0300

    Minor stashed changes pushed

--- web/client.js
+++ web/client.js
@@ -17,7 +17,10 @@ let options2 = Object.assign({}, options);
 let options3 = Object.assign({}, options);
 options3.path = '/unretItems';
 
-let data = ['', '', ''];
+let options4 = Object.assign({}, options);
+options4.path = '/price-list';
+
+let data = ['', '', '', ''];
 let request = http.request(options, (response) => {
   response.setEncoding('utf8');
   response.on('data', (chunk) => {
@@ -38,43 +41,63 @@ request.on('error', e => {
 
 request.end();
 
-let request2 = http.request(options2, (response) => {
-  response.setEncoding('utf8');
-  response.on('data', (chunk) => {
-      data[1] += chunk;
-  });
+// let request2 = http.request(options2, (response) => {
+//   response.setEncoding('utf8');
+//   response.on('data', (chunk) => {
+//       data[1] += chunk;
+//   });
 
-  response.on('close', () => {
-      console.table(JSON.parse(data[1]));
-  })
-});
+//   response.on('close', () => {
+//       console.table(JSON.parse(data[1]));
+//   })
+// });
 
-request2.on('error', e => {
-  console.log('REQUEST2 ERROR:');
-  console.log('\x1b[33mWARNING:\x1b[0m: ');
-  console.log('\x1b[33mWARNING:\x1b[0m: ', e);
-  // exit(e);
-});
+// request2.on('error', e => {
+//   console.log('REQUEST2 ERROR:');
+//   console.log('\x1b[33mWARNING:\x1b[0m: ');
+//   console.log('\x1b[33mWARNING:\x1b[0m: ', e);
+//   // exit(e);
+// });
 
-request2.end();
+// request2.end();
 
 
-let request3 = http.request(options3, (response) => {
-  response.setEncoding('utf8');
-  response.on('data', (chunk) => {
-      data[2] += chunk;
-  });
+// let request3 = http.request(options3, (response) => {
+//   response.setEncoding('utf8');
+//   response.on('data', (chunk) => {
+//       data[2] += chunk;
+//   });
 
-  response.on('close', () => {
-      console.table(JSON.parse(data[2]));
-  })
-});
+//   response.on('close', () => {
+//       console.table(JSON.parse(data[2]));
+//   })
+// });
 
-request3.on('error', e => {
-  console.log('REQUEST3 ERROR:');
-  console.log('\x1b[33mWARNING:\x1b[0m: ');
-  console.log('\x1b[33mWARNING:\x1b[0m: ', e);
-  // exit(e);
-});
+// request3.on('error', e => {
+//   console.log('REQUEST3 ERROR:');
+//   console.log('\x1b[33mWARNING:\x1b[0m: ');
+//   console.log('\x1b[33mWARNING:\x1b[0m: ', e);
+//   // exit(e);
+// });
 
-request3.end();
+// request3.end();
+
+// let request4 = http.request(options3, (response) => {
+//   response.setEncoding('utf8');
+//   response.on('data', (chunk) => {
+//       data[3] += chunk;
+//   });
+
+//   response.on('close', () => {
+//       console.table(JSON.parse(data[2]));
+//   })
+// });
+
+// request4.on('error', e => {
+//   console.log('REQUEST4 ERROR:');
+//   console.log('\x1b[33mWARNING:\x1b[0m: ');
+//   console.log('\x1b[33mWARNING:\x1b[0m: ', e);
+//   // exit(e);
+// });
+
+// request4.end();
